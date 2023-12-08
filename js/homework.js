3468bc19fa44ae7ed331f4bebf0b7e21026ac840
ZiyouddinMominov <muminovz2009@gmail.com>
2023-12-08 22:54:08 +0500

    uyga vazifa tayyor

---
+++ js/homework.js
@@ -0,0 +1,82 @@
+// 1 - TAYYOR
+// let res;
+// function sum(arg1, arg2, arg3) {
+//   if (arg1 > arg2) {
+//     res = arg1;
+//   } else {
+//     res = arg2;
+//   }
+//   if (arg3 > res) {
+//     res = arg3;
+//   }
+// }
+// sum(3, 5, 7);
+// console.log(res);
+// 2 - TAYYOR
+// function sum(son1) {
+//   let res = 0;
+//   let son1b = son1 % 10;
+//   let son2b = ((son1 % 100) - son1b) / 10;
+//   let son3b = son1 / 100 - (son1 % 100) / 100;
+//   if (son1b % 2 == 0) {
+//     res += son1b;
+//   }
+//   if (son2b % 2 == 0) {
+//     res += son2b;
+//   }
+//   if (son3b % 2 == 0) {
+//     res += son3b;
+//   }
+//   console.log(res);
+// }
+// sum(482);
+// 3 - TAYYOR
+// let res;
+// function sum(son) {
+//   let son1 = son % 10;
+//   let son2 = ((son % 100) - son1) / 10;
+//   let son3 = son / 100 - (son % 100) / 100;
+//   return (res = son1 + son2 + son3);
+// }
+// sum(482);
+// console.log(res);
+// 4 - TAYYOR
+// let res = 0;
+// function sum(son) {
+//   let son1 = son % 10;
+//   let son2 = ((son % 100) - son1) / 10;
+//   let son3 = son / 100 - (son % 100) / 100;
+//   if (son1 % 2 == 0) {
+//     res += son1;
+//   }
+//   if (son2 % 2 == 0) {
+//     res += son2;
+//   }
+//   if (son3 % 2 == 0) {
+//     res += son3;
+//   }
+// }
+// sum(484);
+// console.log(res);
+// 5 - TAYYOR
+// let res;
+// function sum(son) {
+//   for (let i = 0; i <= son; i++) {
+//     if (i % 3 != 0 && i % 5 != 0) {
+//       res = i * i;
+//     }
+//   }
+// }
+// sum(20);
+// console.log(res);
+// 6 - TAYYOR
+// let res = 0;
+// function sum(son) {
+//   for (let i = 0; i <= son; i++) {
+//     if (i % 7 == 0 || i % 5 == 0) {
+//       res += 1;
+//     }
+//   }
+// }
+// sum(367);
+// console.log(res);
---
+++ js/practice.js
@@ -0,0 +1,33 @@
+// PRACTICE 1
+// function maxTwoNumbers(arg1, arg2) {
+//   let max;
+//   if (arg1 > arg2) {
+//     max = arg1;
+//   } else {
+//     max = arg2;
+//   }
+
+//   console.log(max);
+// }
+// maxTwoNumbers(3, 5);
+// PRACTICE 2
+// function sum(arg1, arg2) {
+//   return arg1 + arg2;
+// }
+// console.log(sum(5, 7));
+// MASALA 1 TAYYOR
+// function kub(arg1) {
+//   return arg1 ** 3;
+// }
+// console.log(kub(10));
+// MASALA 2 TAYYOR
+// function sum(arg1, arg2) {
+//   return (arg1 + arg2) ** 2;
+// }
+// console.log(sum(2, 4));
+// MASALA 3 TAYYOR
+// ARROW FUNCTION
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
